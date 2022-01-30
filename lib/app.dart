98b5eeb9e8158a602bda7c7715ee4646d1b36c3d
Gökhan Topcu <64787457+JailBreakerTV@users.noreply.github.com>
2022-01-30 02:47:45 +0100

    add useful dependencies and initial routes

---
+++ lib/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:money_tracker/routes/initial_route.dart';
+
+class MoneyTrackerApp extends StatelessWidget {
+  const MoneyTrackerApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Money Tracker',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const InitialPage(),
+    );
+  }
+}
